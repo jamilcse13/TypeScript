7be64e24ba699b9e0c58a96beae0d323067d3b52
jamil <jamil.cse13@gmail.com>
2022-07-31 01:35:10 +0600

    types of typescript

---
+++ src/2type.ts
@@ -0,0 +1,47 @@
+// declare and assign
+let playerName = "Mushfiq";
+
+console.log(playerName);
+
+// playerName = 34; // type error
+
+
+// declare 
+let code;
+// then assign 
+code = 1234;
+code = "25gk3";
+
+
+// type defining and checking
+function multiply(a: number, b: number) {
+    return a * b;
+}
+console.log(multiply(3, 5))
+
+
+
+// type checking for array 
+let arr = ["apple", "banana"]
+// arr.push(36);  //type error
+
+// we can push any value here
+let arr2 = []
+arr2.push(28);
+arr2.push("jamil");
+
+// we can push only push defined types of data
+let arr3 = [24, "string"]
+arr3.push(28);
+arr3.push("jamil");
+// arr3.push(true);  // can not push bool data
+
+
+// type checking for object
+let person = {
+    name: "Mushfiq",
+    age: 34,
+    isCaptain: true
+}
+
+// person.name = 255; //don't match the type
