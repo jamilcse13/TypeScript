86c7d5477bac546f0644af9dfd70b1798feab8fd
jamil <jamil.cse13@gmail.com>
2022-08-14 03:17:50 +0600

    tuples

---
+++ output/12tuples.js
@@ -0,0 +1,8 @@
+"use strict";
+let k = [3, 'hello', {}];
+k[1] = 5;
+// value can be added with strict position type
+// number of elements should be fixed
+// tuple
+let m = [7, 'world', {}];
+// m[1] = 1
---
+++ src/12tuples.ts
@@ -0,0 +1,8 @@
+let k = [3, 'hello', {}]
+k[1] = 5
+
+// value can be added with strict position type
+// number of elements should be fixed
+// tuple
+let m: [number, string, object] = [7, 'world', {}]
+// m[1] = 1
